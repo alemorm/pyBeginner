e8d66bc0a941788870ce127ceb8022f76c995b38
alemorm <alemor@terpmail.umd.edu>
2016-03-24 18:14:13 -0400

    Portmanteaud

--- ex05.py
+++ ex05.py
@@ -2,8 +2,5 @@
 ## Objective: create a function combine(word1,word2); when run, the function should return a portmanteau of the inputted parameters
 
 def combine(word1,word2):
-	word1 = raw_input('Enter the first word: ')
-	word2 = raw_input('Enter the second word: ')
-	print word1 + word2
 
-combine("word1","word2")
+	print word1 + word2
