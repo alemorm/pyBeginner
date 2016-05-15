1adc5de481b5c7140783df3ece8d3cd41320912f
Alejandro Morales <Alejandro Morales>
2016-05-15 16:50:32 -0400

    update

--- ex02.py
+++ ex02.py
@@ -1,2 +1,6 @@
 # Exercise 2
 ## Objective: place all letters of "codeNIH is the best!" in an array AND print array elements 14 to 19
+from array import array
+
+phrase = array('c', 'codeNIH is the best!')
+phrase[14:19]
